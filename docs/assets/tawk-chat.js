fc251ec3d6b454a3073e02ea3bc5321fe7c4575c
brsi4653-wq <brsi4653@gmail.com>
2026-06-28 22:36:26 -0400

    Add files via upload

---
+++ docs/assets/tawk-chat.js
@@ -0,0 +1,14 @@
+window.Tawk_API = window.Tawk_API || {};
+window.Tawk_LoadStart = new Date();
+
+(function () {
+  var script = document.createElement("script");
+  var firstScript = document.getElementsByTagName("script")[0];
+
+  script.async = true;
+  script.src = "https://embed.tawk.to/6a41d8278719f21d5abdf332/1js8j8nba";
+  script.charset = "UTF-8";
+  script.setAttribute("crossorigin", "*");
+
+  firstScript.parentNode.insertBefore(script, firstScript);
+})();
